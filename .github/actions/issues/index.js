ef8da3ac76d64392b9f495dfe79a8539758c7008
pkomara <Pavan_Sai.Komara@genesys.com>
2020-11-01 22:48:03 +0530

    modified octokit

--- .github/actions/issues/index.js
+++ .github/actions/issues/index.js
@@ -1,15 +1,18 @@
 const core = require('@actions/core')
 const github =require('@actions/github')
+const { Octokit } = require("@octokit/action");
+
 
 async function  run()
 {
-// try {
+try {
     const token = core.getInput('token')
     const title = core.getInput('title')
     const body = core.getInput('body')
     const assignees = core.getInput('assignees')
 
-    const octokit =new github(token)
+    // const octokit =new github(token)
+    const octokit =new Octokit()
     const response = await octokit.issues.create({
         // owner : github.context.repo.owner,
         // repo : github.context.repo.repo
@@ -21,9 +24,9 @@ async function  run()
 
     core.setOutput('issue',JSON.stringify(response.data))
 
-// } catch (error) {
-//     core.setFailed(error.message)
-// }
+} catch (error) {
+    core.setFailed(error.message)
+}
 }
 
 run ();
