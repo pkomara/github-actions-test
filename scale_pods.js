bfee0bdd1229eb8b8ac89fe0032db0328882acc6
pkomara <Pavan_Sai.Komara@genesys.com>
2021-06-01 14:57:33 +0530

    test cron

--- scale_pods.js
+++ scale_pods.js
@@ -1,52 +1,54 @@
 const fs = require('fs');
 
-
 let scheduleFile = './schedule_scale_pods.json';
 let dateNow = new Date();
-dateNow.setUTCMinutes(30);
-dateNow.setUTCHours(15);
+// dateNow.setUTCMinutes(30);
+// dateNow.setUTCHours(21);
+let hourNow = dateNow.getUTCHours();
+let minsNow = dateNow.getUTCMinutes();
+
+minsNow=(minsNow>=0 && minsNow<5)?0:minsNow;
+minsNow=(minsNow>=30 && minsNow<35)?30:minsNow;
+
+dateNow.setUTCMinutes(minsNow);
 
 console.log('Time now is '+dateNow.getUTCHours()+":"+dateNow.getUTCMinutes());
 
-function calculateTimeDiffInMins(time1,time2){
-    diffMs=Math.abs(time1-time2)
-    var diffHrs = Math.floor((diffMs % 86400000) / 3600000); 
-    var diffMins = Math.round(((diffMs % 86400000) % 3600000) / 60000); 
-    return (diffHrs*60+diffMins);
-}
 
 try {
     const schedule = fs.readFileSync(scheduleFile, 'utf8');
     const jsonSchedule = JSON.parse(schedule, null, 3);
     // console.log(schedule);
     for ( let service in jsonSchedule ) {
-        let LoadStartTime = new Date();
-        let LoadEndTime = new Date();
 
-        const LoadStartTimeHour = parseInt(jsonSchedule[service]['startTime'].split(":")[0].trim()) ;
-        const LoadStartTimeMins = parseInt(jsonSchedule[service]['startTime'].split(":")[1].trim()) ;
-        const LoadEndTimeHour = parseInt(jsonSchedule[service]['endTime'].split(":")[0].trim()) ;
-        const LoadEndTimeMins = parseInt(jsonSchedule[service]['endTime'].split(":")[1].trim());
+        let LoadStartTimeHour = parseInt(jsonSchedule[service]['startTime'].split(":")[0].trim()) ;
+        let LoadStartTimeMins = parseInt(jsonSchedule[service]['startTime'].split(":")[1].trim()) ;
+        let LoadEndTimeHour = parseInt(jsonSchedule[service]['endTime'].split(":")[0].trim()) ;
+        let LoadEndTimeMins = parseInt(jsonSchedule[service]['endTime'].split(":")[1].trim());
 
-        LoadStartTime.setUTCHours(LoadStartTimeHour);
-        LoadStartTime.setUTCMinutes(LoadStartTimeMins);
-        LoadEndTime.setUTCHours(LoadEndTimeHour);
-        LoadEndTime.setUTCMinutes(LoadEndTimeMins);
+        let timeDiffInMins=(LoadStartTimeHour-hourNow)*60 +LoadStartTimeMins-minsNow;
 
-        let timeDiffInMins=calculateTimeDiffInMins(LoadStartTime,dateNow);
-        console.log('Load start :',LoadStartTime.toUTCString())
-        console.log('Load End : ',LoadEndTime.toUTCString())
-        if ((dateNow <= LoadStartTime) && (timeDiffInMins>=0 && timeDiffInMins<30)){
+        if (timeDiffInMins>=0 && timeDiffInMins<30){
             // pre-scale up the pods as load test is about to begin.
-            console.log("Prescaling to "+ jsonSchedule[service]['preScaleUpPods'] + " pods for service " + service);              
-
+            console.log(jsonSchedule[service]['preScaleUpPods']);
+            if( parseInt(jsonSchedule[service]['actualPods']) < parseInt(jsonSchedule[service]['preScaleUpPods']) ) // donot pre-scale pods when actual pod count is more than preScaleUpPods value
+            {
+                console.log("Prescaling to "+ jsonSchedule[service]['preScaleUpPods'] + " pods for service " + service);
+                // let output = execSync("kubectl scale "+ jsonSchedule[service]['resourceType'] +" -n voice "+service+" --replicas="+jsonSchedule[service]['preScaleUpPods'])
+                // console.log("Prescaled pods for service "+service +" : " +output);                
+            }
+            else{
+                console.log("Cannot prescale as preScaleUpPods value is less than actual pod count")
+            }
         }
+        
+        timeDiffInMins=(LoadEndTimeHour-hourNow)*60 +LoadEndTimeMins-minsNow;
 
-        timeDiffInMins=calculateTimeDiffInMins(LoadEndTime,dateNow);
-
-        if((dateNow >= LoadEndTime) && (timeDiffInMins>=0 && timeDiffInMins<30) ){
+        if(timeDiffInMins>-30 && timeDiffInMins<=0){
             //scale down the pods as load test has ended.
             console.log("Down scaling to "+ jsonSchedule[service]['actualPods'] + " pods for service " + service);
+            // let output = execSync("kubectl scale "+ jsonSchedule[service]['resourceType'] +" -n voice "+service+" --replicas="+jsonSchedule[service]['actualPods']);
+            // console.log("downscaled pods for service "+service +" : " +output);
         }
     }
 
