fb8060e6ed0e6615503c17db4835434ebd34cd7a
pkomara <Pavan_Sai.Komara@genesys.com>
2021-06-10 18:17:17 +0530

    test

--- scale_pods.js
+++ scale_pods.js
@@ -2,38 +2,55 @@ const fs = require('fs');
 
 let scheduleFile = './schedule_scale_pods.json';
 let dateNow = new Date();
-dateNow.setUTCMinutes(40);
-dateNow.setUTCHours(15);
+// dateNow.setUTCMinutes(45);
+// dateNow.setUTCHours(14);
 let hourNow = dateNow.getUTCHours();
 let minsNow = dateNow.getUTCMinutes();
-
+let statefulsetServices= ['voice-ors','voice-rq','voice-sip','voice-sipproxy' ];
 
 console.log('Time now is '+dateNow.getUTCHours()+":"+dateNow.getUTCMinutes());
-
 
 try {
     const schedule = fs.readFileSync(scheduleFile, 'utf8');
     const jsonSchedule = JSON.parse(schedule, null, 3);
     // console.log(schedule);
-    let LoadStartTimeHour = parseInt(jsonSchedule['loadStartTime'].split(":")[0].trim()) ;
-    let LoadStartTimeMins = parseInt(jsonSchedule['loadStartTime'].split(":")[1].trim()) ;
-    let LoadEndTimeHour = parseInt(jsonSchedule['loadEndTime'].split(":")[0].trim()) ;
-    let LoadEndTimeMins = parseInt(jsonSchedule['loadEndTime'].split(":")[1].trim());
+
+    for(let load in jsonSchedule['loadTests'])
+    {
+        let LoadStartTimeHour = parseInt(jsonSchedule['loadTests'][load]['startTime'].split(":")[0].trim()) ;
+        let LoadStartTimeMins = parseInt(jsonSchedule['loadTests'][load]['startTime'].split(":")[1].trim()) ;
+        let LoadEndTimeHour = parseInt(jsonSchedule['loadTests'][load]['endTime'].split(":")[0].trim()) ;
+        let LoadEndTimeMins = parseInt(jsonSchedule['loadTests'][load]['endTime'].split(":")[1].trim());
     
-    for ( let service in jsonSchedule.services ) {
-
         let timeDiffInMins=(LoadStartTimeHour-hourNow)*60 +LoadStartTimeMins-minsNow;
-
-        if (timeDiffInMins>=0 && timeDiffInMins<60){
-            // pre-scale up the pods as load test is about to begin.
-            console.log("Prescaling to "+ jsonSchedule['services'][service]['preScaleUpPods'] + " pods for service " + service);               
+    
+        if (timeDiffInMins>=0 && timeDiffInMins<60)    // pre-scale up the pods as load test is about to begin.
+        {
+            console.log(`${load} load test is about to start`);
+            for ( let service in jsonSchedule['services'] ) 
+            {
+                console.log("Prescaling to "+ jsonSchedule['services'][service]['preScaleUpPods'] + " pods for service " + service);
+                resourceType = statefulsetServices.includes(service) ? 'statefulset':'deployment';
+                console.log("kubectl scale "+ resourceType +" -n voice "+service+" --replicas="+jsonSchedule['services'][service]['preScaleUpPods'])              
+            }
         }
         
         timeDiffInMins=(hourNow-LoadEndTimeHour)*60 +minsNow-LoadEndTimeMins;
-
-        if(timeDiffInMins>=0 && timeDiffInMins<60){
-            //scale down the pods as load test has ended.
-            console.log("Down scaling to "+ jsonSchedule['services'][service]['replicaCount'] + " pods for service " + service);
+    
+        if(timeDiffInMins>=0 && timeDiffInMins<60)    //scale down the pods as load test has ended.
+        {
+            console.log(`${load} load test is about to end`);
+            for ( let service in jsonSchedule['services'] ) 
+            {
+                console.log("Down scaling to "+ jsonSchedule['services'][service]['replicaCount'] + " pods for service " + service);
+                resourceType = statefulsetServices.includes(service) ? 'statefulset':'deployment';
+                console.log("kubectl scale "+ resourceType +" -n voice "+service+" --replicas="+jsonSchedule['services'][service]['replicaCount']);         
+            }
+            if(! jsonSchedule['loadTests'][load].recursive){
+                //remove the load information from the json file as the load is not recursive
+                delete jsonSchedule['loadTests'][load];
+                fs.writeFileSync(scheduleFile,JSON.stringify(jsonSchedule,null,3),'utf8');
+            }
         }
     }
 
--- schedule.js
+++ schedule.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 // let inputs = process.argv[2];
-let inputs ='{"load_end_time": "21:00","load_start_time": "21:45","location": "westus2","pre_scaleup_pods": "8","service_name": "voice-ors"}';
+let inputs ='{"load_end_time": "21:00","load_start_time": "21:45","location": "westus2","load_test_name": "load3","is_load_recursive":false}';
 
 inputsJSON = JSON.parse(inputs,null,3);
 // console.log(inputsJSON);
@@ -10,20 +10,18 @@ let scheduleFile = './schedule_scale_pods.json';
 try {
     const schedule = fs.readFileSync(scheduleFile, 'utf8');
     const jsonSchedule = JSON.parse(schedule, null, 3);
-    let service = inputsJSON['service_name'];
+    let load = inputsJSON['load_test_name'];
 
-    if(jsonSchedule['services'][service]==undefined){
-        jsonSchedule['services'][service] = JSON.parse("{}",null,3);
-        jsonSchedule['services'][service]['resourceType'] = 'statefulset';
+    if(jsonSchedule['loadTests'][load]==undefined){
+        jsonSchedule['loadTests'][load]= JSON.parse("{}",null,3);
     }
 
     //update json values
-    jsonSchedule['services'][service]['preScaleUpPods'] = inputsJSON['pre_scaleup_pods'] ? inputsJSON['pre_scaleup_pods']:jsonSchedule['services'][service]['preScaleUpPods'];
-    jsonSchedule['loadStartTime'] = inputsJSON['load_start_time'] ? inputsJSON['load_start_time']:jsonSchedule['loadStartTime'];
-    jsonSchedule['loadEndTime'] = inputsJSON['load_end_time'] ? inputsJSON['load_end_time']:jsonSchedule['loadEndTime'];
-    // jsonSchedule['services'][service]['replicaCount'] = parseInt(replicaCount);
+    jsonSchedule['loadTests'][load]['startTime'] = inputsJSON['load_start_time'] ? inputsJSON['load_start_time']:jsonSchedule['loadTests'][load]['startTime'];
+    jsonSchedule['loadTests'][load]['endTime'] = inputsJSON['load_end_time'] ? inputsJSON['load_end_time']:jsonSchedule['loadTests'][load]['endTime'];    
+    jsonSchedule['loadTests'][load]['recursive'] = inputsJSON['is_load_recursive'];
     
-    console.log(`updating schedule json \n${service}:`,jsonSchedule['services'][service]);
+    console.log(`updating schedule json for load tests`);
     
     //write updated json to the file
     fs.writeFileSync(scheduleFile,JSON.stringify(jsonSchedule,null,3),'utf8');
