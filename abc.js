fdff481ae404c317343312d0f9817b5eedd671f8
pkomara <Pavan_Sai.Komara@genesys.com>
2021-09-06 20:20:00 +0530

    test

--- abc.js
+++ abc.js
@@ -11,7 +11,7 @@ try {
     const tenants = fs.readFileSync(tenantsFile, 'utf8');
     const tenantsJSON = JSON.parse(tenants, null, 3);
     const tenantData = JSON.parse(tenant_data,null,3);
-    const tenant_found = false;
+    let tenant_found = false;
     tenant_key = Object.keys(tenantData)[0];
     tenant_value = tenantData[tenant_key];
     if(tenant_value['key']){
